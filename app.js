7fd2b2c0e7a17ffabc0a1a611efc8d76a112a502
kimsos0723 <kimsos0723@gmail.com>
2022-03-26 12:08:23 +0900

    add use file

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-import {convert_text} from './module_from_God-sejong/mainscript.js';
-convert_text('요번에 오랜만에 외국여행을 친구들이랑 왔는데 여기 집주인이 아주 아니에요. 가격도 그렇게 싼 편이 아닌데 서비스가 엉망이고요, 화장실도 더럽고 우리가 뭐라 하니 화만 내네요 ㅠ.ㅠ 여기 주변 음식점도 다 맛 없고 분위기도 별로에요.\n\n여기 리뷰 잘 써주면 할인해준다고는 해서 쓰고있는데 이 리뷰 보시고 다들 잘 거르시길 바랍니다...');
+import * as fs from 'fs'
+import { convert_text } from './module_from_God-sejong/mainscript.js'
+
+fs.readFile('./input.txt', 'utf8', function (err, data) {
+    if(err){
+        console.log(err)
+        return;
+    }
+    fs.writeFileSync('output.txt', convert_text(data))
+})
